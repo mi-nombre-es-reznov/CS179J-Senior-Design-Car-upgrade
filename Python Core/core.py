84116ab903352b8546c44ffde697b3e7a849b357
Lawrence Yuen <lawrencey@lawrences-mbp.wdynp.ucr.edu>
2019-05-14 21:52:13 -0700

    Added hx711 module and inintegrated with core

--- Python Core/core.py
+++ Python Core/core.py
@@ -1,5 +1,15 @@
 import sys
 import csv
+import time
+import RPi.GPIO as GPIO
+from hx711 import HX711
+
+hx = HX711(5, 6)
+hx.set_reading_format("MSB", "MSB")
+hx.set_reference_unit(-422.5)
+hx.reset()
+hx.tare()
+
 
 userFingerPrint = []
 userPassword = []
@@ -7,13 +17,16 @@ ItemPairs = []
 Finish = ""
 Done = ""
 
+
+
 def menu():
-    print ("Welcome to the secure fridge \n")
-    print ("1) Add User \n")
-    print ("2) Remove User \n")
-    print ("3) Add/Remove Item \n")
-    print ("4) Add Barcode \n")
-    print ("5) Remove Barcode \n")
+    CLS()
+    print ("Welcome to the secure fridge ")
+    print ("1) Add User ")
+    print ("2) Remove User ")
+    print ("3) Add/Remove Item ")
+    print ("4) Add Barcode ")
+    print ("5) Remove Barcode ")
     print ("6) Test I/O Menu")
     print ("Choice: ")
 
@@ -35,6 +48,8 @@ def menu():
         removeBarcode()
     elif userInput == "6":
         testIOMenu()
+    elif userInput == "7":
+        GetWeight()
     else:
         print("Wrong choice")
         menu()
@@ -44,6 +59,7 @@ def viewBarcodes():
     print ("In view barcodes")
 
 def addUser():
+    fingerPrintFunction()
     fout = open("Users.txt", 'a')
     print("Add new fingerprint: ")
     newFingerprint = input()
@@ -54,6 +70,7 @@ def addUser():
     userData = newFingerprint + "\t" + newPass + "\n"
     fout.write(userData)
     fout.close()
+    menu()
 
 def removeUser():
     print("Removing user")
@@ -74,17 +91,37 @@ def addBarcode():
     userData = newBarcodeItem + "\t" + newItemName + "\n"
     fout.write(userData)
     fout.close()
+    menu()
 
 
 def removeBarcode():
-    print("Removing barcode")
+    DDD = {name: (upc) for name, upc in ItemPairs}
+    print("Enter barcode: (-1 to exit)")
+    userRemoveBarcode = input()
+    if userRemoveBarcode == "-1":
+        menu()
+    try:
+        barcodeName = DDD[userRemoveBarcode]
+        itemToRemove = userRemoveBarcode + "\t" + barcodeName
+        with open("ItemBarcodes.txt", "r") as f:
+            lines = f.readlines()
+        with open("ItemBarcodes.txt", "w") as f:
+            for line in lines:
+                if line.strip("\n") != itemToRemove:
+                    f.write(line)
+        print("Item removed")
+        menu()
+    except KeyError:
+        print("Barcode does not exist")
+        removeBarcode()
+
 
 def testIOMenu():
     CLS()
-    print ("Welcome to the I/O Testing Menu \n")
-    print ("0) Exit \n")
-    print ("1) Test solenoid open \n")
-    print ("2) Test solenoid close \n")
+    print ("Welcome to the I/O Testing Menu ")
+    print ("0) Exit ")
+    print ("1) Test solenoid open ")
+    print ("2) Test solenoid close ")
     print ("Choice: ")
 
 
@@ -100,7 +137,7 @@ def fingerPrintFunction():
         print("Enter password: ")
         userPassword = input()
         if (isMatch(userFingerPrint, userPassword)):
-            print("Access granted \n")
+            print("Access granted")
             return True
         else:
             print("\n Access denied \n")
@@ -121,21 +158,26 @@ def programExit():
 
 def barCodeFunction():
     D = {upc: (name) for upc, name in ItemPairs}
+    DD = {name: (upc) for name, upc in ItemPairs}
     userBarcodeinput = "entry"
-    while userBarcodeinput != Done:
-        print("Scan Barcode: ")
+    doneCode = DD["Done"]
+    while userBarcodeinput != doneCode:
+        print("Scan Done if you are finished or \nScan Barcode: ")
         userBarcodeinput = input()
         userBarcodeinput = str(userBarcodeinput)
+        if userBarcodeinput == doneCode:
+            menu()
         try:
             foundItem = D[userBarcodeinput]
             print("Desposit/Withdrawl your " + foundItem)
-            print("\n")
             Transaction(foundItem)
 
         except KeyError:
             print("Not in database")
             print("Press 'c' to continue")
             userBarcodeinput = input()
+
+    menu()
 
 
 def LoadUser():
@@ -168,6 +210,8 @@ def LoadBarcodes():
 def Transaction(foundItem):
     D = {name: (upc) for name, upc in ItemPairs}
     startWeight = GetWeight()
+    startWeight1 = str(startWeight)
+    print("Start weight is: " + startWeight1)
     print("Scan \"Finish\" when you are done")
     userInput = "entry"
     finishCode = D["Finish"]
@@ -176,11 +220,34 @@ def Transaction(foundItem):
         if (userInput != finishCode):
             print("Wrong code, scan Finish: ")
     endWeight = GetWeight()
+    endWeight1 = str(endWeight)
+    print("End weight is: " + endWeight1)
     netWeightChange = endWeight - startWeight
-    programExit()
+    netWeightChange1 = str(netWeightChange)
+    print("Net weight is: " + netWeightChange1)
+    barCodeFunction()
 
 def GetWeight():
-    return 1
+    while True:
+        try:
+            val = hx.get_weight(5)
+      #      print(val)
+            hx.power_down()
+            hx.power_up()
+            time.sleep(0.1)
+            time.sleep(3)
+            if val < 0:
+                return 0
+      #      userGetWeight = input()
+            return val
+            
+        except(KeyboardInterrupt, SystemExit):
+            print ("Cleaning...")
+            GPIO.cleanup()
+            print ("Bye!")
+            print("getweight work")
+            sys.exit()
+    
 
 def CLS():
     print ("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
